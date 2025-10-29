67edcf7be8f15b43189c4ee0a591df607205c5d1
Rudra <105378849+nilerudra@users.noreply.github.com>
2025-10-29 22:33:50 +0530

    Minor issue solved

--- Backend/src/utils/llmService.js
+++ Backend/src/utils/llmService.js
@@ -2,9 +2,9 @@ const axios = require("axios");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const contentWithLLM = async (content) => {
   const genAI = new GoogleGenerativeAI(
-    "AIzaSyBZDzPftDUgmwANqQIw9mGvXw0eVLUVZRs"
+    "AIzaSyCvISj9B2-DtuzhfGqtlcPNDiFuiizlmh8"
   );
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
   const prompt = `You are an intelligent categorization system. Analyze the following content and categorize it into one of the predefined categories. Provide only one word for the category.\n\nContent:\n${content}`;
 
