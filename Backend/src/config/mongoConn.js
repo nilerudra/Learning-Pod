96a08f28693ddeafbebdc6dada805898bcf3a56d
om kathar <omkathar15@gmail.com>
2025-11-14 18:31:12 +0530

    backend minor change

--- Backend/src/config/mongoConn.js
+++ Backend/src/config/mongoConn.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connectDB = async () => {
-  console.log(process.env.MONGO_URI);
+ 
   try {
     await mongoose
       .connect(process.env.MONGO_URI, {})
